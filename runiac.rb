75340dd4f795f53946fc8f2e2e995f135bb21caa
goreleaserbot <goreleaser@optum.com>
2021-02-06 13:07:13 -0600

    Brew formula update for runiac version v0.0.1-beta1

--- runiac.rb
+++ runiac.rb
@@ -11,15 +11,15 @@ class Runiac < Formula
 
   if OS.mac?
     url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta1/runiac_0.0.1-beta1_Darwin_x86_64.tar.gz"
-    sha256 "13805c539be9ba5ef1b115b67c5b0576659eb187a90e6c76861e332c20271a32"
+    sha256 "02d2bb672dc9117ef3636add837cfbdd8d0b16a9e5aad4381cc36b90e2690b45"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta1/runiac_0.0.1-beta1_Linux_x86_64.tar.gz"
-    sha256 "fbb1326d9ca20543c2e62ae9f9511a7e094aec2b0922cef69fc2ff14a6fac0d9"
+    sha256 "97b52aca881aa948ecc558f5c8dc0212c0d35da9a6ccc82337cb368a41e1d4a9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta1/runiac_0.0.1-beta1_Linux_arm64.tar.gz"
-    sha256 "d7d379431143876abd33fd831d0d83240e5c06c2efe3838078b78ede15e41ee6"
+    sha256 "53ebac8d329ff6f9ce08d0b1fc68acc21d8f55465c02a2ab174300b45a54386e"
   end
 
   def install
