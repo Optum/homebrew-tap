fd34bbe6741f2deb1fbe4728c3a62bb874ec9b41
goreleaserbot <goreleaser@optum.com>
2021-02-08 17:39:44 -0600

    Brew formula update for runiac version v0.0.1-beta2

--- Formula/runiac.rb
+++ Formula/runiac.rb
@@ -5,21 +5,21 @@
 class Runiac < Formula
   desc "Run IaC Anywhere with Ease"
   homepage "https://github.com/optum/runiac"
-  version "0.0.1-beta1"
+  version "0.0.1-beta2"
   license "Apache2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta1/runiac_0.0.1-beta1_Darwin_x86_64.tar.gz"
-    sha256 "e128a344db0f34f025714b8e7cc19c87496d52518fdc92315d2b88ea5a44b550"
+    url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta2/runiac_0.0.1-beta2_Darwin_x86_64.tar.gz"
+    sha256 "ec3434da4f525e857a33cdbb31776aba91df7f3d5d36cd958727c9fdff57bdf4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta1/runiac_0.0.1-beta1_Linux_x86_64.tar.gz"
-    sha256 "f1112014ca3ea09f71765516618a83c5aa5ea249403765ca15a1533435a4e16f"
+    url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta2/runiac_0.0.1-beta2_Linux_x86_64.tar.gz"
+    sha256 "b13332d761a2d7d5437665d63939512f0a57087313b93ccf2f567be10acd1510"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta1/runiac_0.0.1-beta1_Linux_arm64.tar.gz"
-    sha256 "431906c516bc4a4bddc45122f4cb2d698af2dc1a37b6f2143752a32cc7236dce"
+    url "https://github.com/Optum/runiac/releases/download/v0.0.1-beta2/runiac_0.0.1-beta2_Linux_arm64.tar.gz"
+    sha256 "e57e99490cab954fd4033e04d7533699a68a08435918447d9f16c2182b61b3af"
   end
 
   def install
