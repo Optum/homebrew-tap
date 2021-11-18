49517d86b4d48f86b202f388781a4a21b175e670
goreleaserbot <goreleaser@optum.com>
2021-11-18 15:20:39 -0500

    Brew formula update for runiac version v0.0.11

--- Formula/runiac.rb
+++ Formula/runiac.rb
@@ -5,34 +5,45 @@
 class Runiac < Formula
   desc "Run IaC Anywhere with Ease"
   homepage "https://runiac.io"
-  version "0.0.10"
+  version "0.0.11"
   license "Apache2"
-  bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/Optum/runiac/releases/download/v0.0.10/runiac_0.0.10_darwin_arm64.tar.gz"
-      sha256 "5f77f83575be254a277f074a759db2751986c4d7c5eb1df27fa5367373620620"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/Optum/runiac/releases/download/v0.0.10/runiac_0.0.10_darwin_x86_64.tar.gz"
-      sha256 "6218629b77e4c8cc3aca7fb5371f903a420720a5045c7324fd965ea6d30ce646"
+      url "https://github.com/Optum/runiac/releases/download/v0.0.11/runiac_0.0.11_darwin_x86_64.tar.gz"
+      sha256 "d34aa53d872aa3e0ff37ccff00ff295abc92222e34f0ecf23d47700baee34ca2"
+
+      def install
+        bin.install "runiac"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/Optum/runiac/releases/download/v0.0.11/runiac_0.0.11_darwin_arm64.tar.gz"
+      sha256 "107bdbc480bbde568c6cd4b391c627ed54e86e78455d0457c1e1e87a2e77d3e7"
+
+      def install
+        bin.install "runiac"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Optum/runiac/releases/download/v0.0.10/runiac_0.0.10_linux_arm64.tar.gz"
-      sha256 "da2193e5fd45ca18160985ad9e4e63d3d6aeeca8d8a46ee55d2d4ac1d9d2a043"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/Optum/runiac/releases/download/v0.0.10/runiac_0.0.10_linux_x86_64.tar.gz"
-      sha256 "84b07a30e2d64f3cfe594da17ec6d65235646aa67454fa707e9fdd99acbe6625"
-    end
-  end
+      url "https://github.com/Optum/runiac/releases/download/v0.0.11/runiac_0.0.11_linux_x86_64.tar.gz"
+      sha256 "73243519816ef6a3c7ebf16d5e7d08f45a0c0b60abdd678ffad2a4d4d50ab353"
 
-  def install
-    bin.install "runiac"
+      def install
+        bin.install "runiac"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Optum/runiac/releases/download/v0.0.11/runiac_0.0.11_linux_arm64.tar.gz"
+      sha256 "ad2ad6bee9ffcc36312caf8729c3e77bd4472f00e62797ef94b78dbb9c701105"
+
+      def install
+        bin.install "runiac"
+      end
+    end
   end
 
   test do
